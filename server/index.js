96e56d1e47ab5ba0010930282f72e231eaa0b3a7
Pier Davis <pieramelia2@gmail.com>
2021-11-12 18:53:46 -0500

    updates

--- server/index.js
+++ server/index.js
@@ -22,4 +22,36 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["Something good is headed your way",
+					 "Fortune favors the brave",
+					 "Plan for many pleasures ahead",
+           "Your persistence will pay off",
+           "One ending is another beginning",
+           "Exploration will nourish your soul",
+  ];
+
+  // choose random fortune
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+  
+});
+
+app.get("/api/joke", (req, res) => {
+  const jokes = ["What do you call a fish wearing a bowtie? Sofishticated.",
+					 "If April showers bring May flowers, what do May flowers bring? Colonizers",
+					 "What did the ocean say to the beach? Nothing it just waved.",
+           "What has more letters than the alphabet? The post office",
+  ];
+
+  // choose random fortune
+  let randomIndex = Math.floor(Math.random() * jokes.length);
+  let randomJoke = jokes[randomIndex];
+
+  res.status(200).send(randomJoke);
+  
+});
+
 app.listen(4000, () => console.log("Server running on 4000"));
